a1a988be5c5e83218654ca9cd115cdb6c7c02911
DVorortnoy <dimchik505@gmail.com>
2023-08-22 13:28:18 +0300

    add  card

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,19 @@
+import s from "./Card.module.scss";
+import bckgrd from "../../assets/images/bckgrd.png";
+import logo from "../../assets/images/logo.png";
+
+export const Card = () => {
+  return (
+    <section className={s.wrapper}>
+      <div className={s.middleline}></div>
+      <img src="https://xsgames.co/randomusers/avatar.php?g=male" className={s.avatar}/>
+      <div className={s.circle}></div>
+
+      <img src={logo} className={s.logo} />
+      <img src={bckgrd} className={s.bckgrd} />
+      <p className={s.tweets}> 777 tweets</p>      
+      <p className={s.followers}>100,500 Followers</p>
+      <button type="button" className={s.btn_nofollow}>Follow</button>
+    </section>
+  );
+};
